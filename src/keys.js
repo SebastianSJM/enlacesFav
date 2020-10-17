f92c7a33114fc2df8cd4d3b174918e7f913a005d
SebastianSJM <59491126+SebastianSJM@users.noreply.github.com>
2020-10-17 18:40:01 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -2,10 +2,10 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'enlaces'
+        host: 'us-cdbr-east-02.cleardb.com',
+        user: 'b3388a370dabc5',
+        password: 'a6afe9cd',
+        database: 'heroku_a6c884cac3769d1'
     }
 
 };
